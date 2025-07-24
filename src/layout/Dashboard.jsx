c39be0e42a811571c704f1d2b2d717e888a26a54
Your Name <your.email@example.com>
2025-07-24 12:35:59 +0300

    makes changes on the layout

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,29 +1,3 @@
-    import React from 'react'
-
-function Dashboard() {
-  return (
-    <>
-    <nav>
-      <ul>
-        <ul>
-            <li><NavLink to={"./dashboard/about"}>Income</NavLink></li>
-            <li><NavLInk to={"./dashboard/country details"}>Expenses</NavLInk></li>
-            <li><NavLink to={"./dashboard/country list"}>Savings</NavLink></li>
-        </ul>
-      </ul>Content
-    </nav>
-    <div className='Content'>
-    <Outlet/>
-    </div>
-    </>
-  )
-}
-
-export default Dashboard
-
-    import React from 'react'
-    import { NavLink, Outlet } from 'react-router-dom'
-
 function Dashboard() {
   return (
     <>
